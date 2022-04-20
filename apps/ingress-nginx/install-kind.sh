26f8c95f519090302c8286a60f0b511ea221c2dc
Neil Deng <neil.deng.g@gmail.com>
2022-04-20 23:24:46 +0800

    調整取得 control-plane 的 INTERNAL_IP 方式

--- apps/ingress-nginx/install-kind.sh
+++ apps/ingress-nginx/install-kind.sh
@@ -3,4 +3,5 @@ set -e
 
 cd "$(dirname "$0")"
 
-kubectl -n ingress-nginx patch svc ingress-nginx-controller -p '{"spec":{"externalIPs":["172.21.0.2","172.21.0.3","172.21.0.4"]}}'
+INTERNAL_IP=$(kubectl get nodes -o wide | grep control-plane | awk '{print $6}')
+kubectl -n ingress-nginx patch svc ingress-nginx-controller -p '{"spec":{"externalIPs":["'"$INTERNAL_IP"'"]}}'
