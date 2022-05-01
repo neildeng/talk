7f152a7432dfad3aeaafbf2ff9dbfa09abef58de
Neil Deng <neil.deng.g@gmail.com>
2022-05-01 15:44:07 +0800

    整合 cert-manager 自簽模式

---
+++ tls/internalCA/install.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# "---------------------------------------------------------"
+# "-                                                       -"
+# "-  Create CA、Secret, and ClusterIssuer                 -"
+# "-                                                       -"
+# "---------------------------------------------------------"
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+WORKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$WORKDIR"
+
+CAROOT=. mkcert -install
+
+kubectl create secret tls ca-key-pair \
+   --cert=rootCA.pem \
+   --key=rootCA-key.pem \
+   --namespace=cert-manager
+
+kubectl apply -f - <<EOF
+apiVersion: cert-manager.io/v1alpha2
+kind: ClusterIssuer
+metadata:
+  name: ca-issuer
+spec:
+  ca:
+    secretName: ca-key-pair
+EOF
---
+++ tls/internalCA/uninstall.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# "---------------------------------------------------------"
+# "-                                                       -"
+# "-  Remove k8s.edu records                               -"
+# "-                                                       -"
+# "---------------------------------------------------------"
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+WORKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$WORKDIR"
+
+CAROOT=. mkcert -uninstall
